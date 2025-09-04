c73a8e7fbd332a315e76fd2b35304d1a489a521d
varun9797 <verma9797@gmail.com>
2025-09-04 22:51:23 +0200

    emv changes

--- Angular-Client/src/environments/environment.prod.ts
+++ Angular-Client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: '',
-    socketUrl: ''
+    apiUrl: 'https://berlin-production.up.railway.app',
+    socketUrl: 'https://berlin-production.up.railway.app'
 };
